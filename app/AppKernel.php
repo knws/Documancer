b4cd24c3d7285dd6574dd6f55b7f4605cf803f63
barif <w3db@yandex.ru>
2013-07-08 17:56:13 +0300

    test default controller

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Totalcan\DocumancerBundle\TotalcanDocumancerBundle(),
+            new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
